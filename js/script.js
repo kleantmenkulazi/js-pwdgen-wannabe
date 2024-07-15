6689537f5ca0e33bfbfff961f4270dfed4251f67
kleantmenkulazi <kleantmenkulazi@gmail.com>
2024-07-15 04:52:57 +0200

    Esercio completato prima versione

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.writeln('La tua password generata è: <br>');
+
+let nomeUtente = prompt("Inserisci il tuo nome"); 
+let cognomeUtente = prompt("Inserisci il cognome"); 
+let coloreUtente = prompt("Qual è il tuo colore preferito?"); 
+
+document.writeln(` ${nomeUtente}${cognomeUtente}${coloreUtente} `);
+
+
+
+
+
